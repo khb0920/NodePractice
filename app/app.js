471d450a427125cf84a29a3345023176d746c266
kimhongbae <smr1995@naver.com>
2021-07-07 04:01:05 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,8 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`));
+
 app.use("/", home); //use -> 미들웨어 사용하게 해줌 
 
 module.exports = app;
