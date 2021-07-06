b378c5a70e0c09d8ff77255cfb9702fd854d31b2
kimhongbae <smr1995@naver.com>
2021-07-07 05:03:18 +0900

    백엔드에 로그인기능 구현

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 "use strict";
+
 //모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = 3000;
@@ -13,7 +15,10 @@ app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/src/public`));
 
-app.use("/", home); //use -> 미들웨어 사용하게 해줌 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//URL통해 전달되는 데이터에 한글, 공백 같은 문자가 포함될 경우 제대로 인식되지 않는 문제해결
 
+app.use("/", home); //use -> 미들웨어 사용하게 해줌 
 module.exports = app;
 
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
-router.get("/", ctrl.hello);
-router.get("/login", ctrl.login);
-router.post("/login", ctrl.login);
+router.get("/", ctrl.output.hello);
+router.get("/login", ctrl.output.login);
+router.post("/login", ctrl.process.login);
+
 
 module.exports = router; //모듈화하여 다른곳에서 사용가능하게
