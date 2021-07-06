5694d20ddc79fe47a0ed0f1427b5d902b670ad45
kimhongbae <smr1995@naver.com>
2021-07-07 04:26:48 +0900

    로그인을 요청하는 fetch생성

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.hello);
-
 router.get("/login", ctrl.login);
+router.post("/login", ctrl.login);
 
 module.exports = router; //모듈화하여 다른곳에서 사용가능하게
